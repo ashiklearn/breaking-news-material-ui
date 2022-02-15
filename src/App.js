6c335a682c41b688c0caed063884a815241e74d4
ashiklearn <ashiklearn1996@gmail.com>
2022-02-15 21:48:18 +0600

    React  material ui breaking news done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import News from './components/News/News';
 
 function App() {
+  const [articles,setArticles] = useState([]);
+//   useEffect(() => {
+// const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=9ba36bd62f834c478ed36d81cce7c27d';
+// fetch(url)
+// .then(response => response.json())
+// .then(data => setArticles(data.articles))
+//   },[])
+  useEffect(() =>{
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=9ba36bd62f834c478ed36d81cce7c27d';
+axios(url)
+.then(data => setArticles(data.data.articles))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> Headlines: {articles.length}</h2>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
     </div>
   );
 }
